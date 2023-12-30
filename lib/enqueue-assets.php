3d93fcedc2d8b1b317d13554285da2957767698d
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2023-12-31 00:32:42 +0800

    up

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -2,7 +2,9 @@
 function dionnietheme_assets()
 {
     wp_enqueue_style('dionnietheme-stylesheet',  get_template_directory_uri() . '/dist/css/bundle.min.css');
-    wp_enqueue_script('dionnietheme-stylesheet',  get_template_directory_uri() . '/dist/js/bundle.min.js');
+    wp_enqueue_style('dionnietheme-bootstrap',  get_template_directory_uri() . '/dist/vendors/bootstrap-5.0.2-dist/css/bootstrap.min.css');
+    wp_enqueue_script('dionnietheme-scripts',  get_template_directory_uri() . '/dist/js/bundle.min.js');
+    wp_enqueue_style('dionnietheme-scripts',  get_template_directory_uri() . '/dist/vendors/bootstrap-5.0.2-dist/js/bootstrap.min.js');
 }
 
 
