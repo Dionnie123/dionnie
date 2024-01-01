11b5ec7ef544d7e3b63fa3c2bac636593478d935
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-01-01 16:20:39 +0800

    up

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -4,8 +4,8 @@ function _dionnietheme_assets()
 {
 
     wp_enqueue_style('dionnietheme-bootstrap',  get_template_directory_uri() . '/dist/vendors/bootstrap-5.0.2-dist/css/bootstrap.min.css');
-    wp_enqueue_style('dionnietheme-stylesheet',  get_template_directory_uri() . '/dist/css/bundle.css'); //this must be the last css to be added
-    wp_enqueue_script('dionnietheme-scripts',  get_template_directory_uri() . '/dist/js/bundle.js', array('jquery'), '1.0.0', true);
     wp_enqueue_style('dionnietheme-scripts',  get_template_directory_uri() . '/dist/vendors/bootstrap-5.0.2-dist/js/bootstrap.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_style('dionnietheme-stylesheet',  get_template_directory_uri() . '/dist/css/bundle.css'); //this must be the last css to be added
+    wp_enqueue_script('dionnietheme-scripts',  get_template_directory_uri() . '/dist/js/bundle.js', array('jquery'), '1.0.0', true); //this must be the last js to be added
 }
 add_action('wp_enqueue_scripts', '_dionnietheme_assets');
