4164ed0806da0f41576a3c1f13f7323ad5cb39ec
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-01-15 19:55:20 +0800

    up

--- template-parts/post/loop.php
+++ template-parts/post/loop.php
@@ -19,25 +19,25 @@ $query = new WP_Query($args); ?>
         <div class="row">
             <div class=" <?php echo is_active_sidebar('primary-sidebar') ?  'col-lg-9' : 'col-lg-12'   ?> ">
                 <?php if ($query->have_posts()) : ?>
-                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 gx-3 gy-3">
-                        <?php while ($query->have_posts()) : $query->the_post(); ?>
-                            <?php get_template_part('template-parts/post/content'); ?>
-                        <?php endwhile; ?>
-                    </div>
-                    <?php bootstrap_pagination($query, $args) ?>
-                    <?php do_action('_themename_after_pagination') ?>
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 gx-3 gy-3">
+                    <?php while ($query->have_posts()) : $query->the_post(); ?>
+                    <?php get_template_part('template-parts/post/content', 'index'); ?>
+                    <?php endwhile; ?>
+                </div>
+                <?php bootstrap_pagination($query, $args) ?>
+                <?php do_action('_themename_after_pagination') ?>
                 <?php else : ?>
-                    <div class="container no-posts">
-                        <h3><?php _e(apply_filters('_themename_no_posts_text',   esc_html("No Posts Found")), '_themename')  ?>
-                        </h3>
-                    </div>
+                <div class="container no-posts">
+                    <h3><?php _e(apply_filters('_themename_no_posts_text',   esc_html("No Posts Found")), '_themename')  ?>
+                    </h3>
+                </div>
                 <?php endif; ?>
             </div>
 
             <?php if (is_active_sidebar('primary-sidebar')) { ?>
-                <div class="col-lg-3">
-                    <?php get_sidebar(); ?>
-                </div>
+            <div class="col-lg-3">
+                <?php get_sidebar(); ?>
+            </div>
             <?php } ?>
         </div>
     </div>
