ef79cfb630a30e9abc118d5c8612f2601500185f
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-02-24 18:14:27 +0800

    up

--- src/lib/custom_post_types/books.php
+++ src/lib/custom_post_types/books.php
@@ -37,8 +37,11 @@ class Book
 class BookDetailMetaBox
 {
 
-    protected  $author = "_themename_book_author";
-    protected  $published_date = "_themename_book_published_date";
+    protected  $author = "__themename_book_author";
+    protected  $published_date = "__themename_book_published_date";
+    protected  $layout = "__themename_book_layout";
+    protected  $nonce_action = "_themename_book_detail_metabox_update";
+    protected  $nonce_field = "_themename_book_detail_metabox_nonce";
 
     public function __construct()
     {
@@ -66,20 +69,26 @@ class BookDetailMetaBox
         // Retrieve existing meta values for the book
         $author = get_post_meta($post->ID, $this->author, true);
         $published_date = get_post_meta($post->ID, $this->published_date, true);
+        $layout = get_post_meta($post->ID, $this->layout, true);
 
         // Output the HTML for the meta box
 
-        wp_nonce_field('_themename_book_detail_metabox_update', '_themename_book_detail_metabox_nonce');
+        wp_nonce_field($this->nonce_action, $this->nonce_field);
 ?>
-<label for="<?php echo $this->author; ?>">Author:</label>
-<input class="widefat" type="text" id="<?php echo $this->author; ?>" name="<?php echo $this->author; ?>"
-    value="<?php echo esc_attr($author); ?>" />
+        <label for="<?php echo $this->author; ?>">Author:</label>
+        <input class="widefat" type="text" id="<?php echo $this->author; ?>" name="<?php echo $this->author; ?>" value="<?php echo esc_attr($author); ?>" />
 
-<br />
+        <br />
 
-<label for="<?php echo $this->published_date; ?>">Published Date:</label>
-<input class="widefat" type="text" id="<?php echo $this->published_date; ?>" name="<?php echo $this->published_date; ?>"
-    value="<?php echo esc_attr($published_date); ?>" />
+        <label for="<?php echo $this->published_date; ?>">Published Date:</label>
+        <input class="widefat" type="text" id="<?php echo $this->published_date; ?>" name="<?php echo $this->published_date; ?>" value="<?php echo esc_attr($published_date); ?>" />
+
+        <label for="<?php echo $this->layout; ?>">Layout:</label>
+        <select class="widefat" name="<?php echo $this->layout; ?>" id="<?php echo $this->layout; ?>" value="<?php echo esc_attr($layout); ?>">
+            <option <?php selected($layout, "full") ?> value="full">Full Width</option>
+            <option <?php selected($layout, "sidebar") ?> value="sidebar">Post with Sidebar</option>
+        </select>
+
 <?php
     }
 
@@ -90,14 +99,11 @@ class BookDetailMetaBox
             return;
         }
 
-          if (!isset($_POST['_themename_book_detail_metabox_nonce']) || !wp_verify_nonce($_POST['_themename_book_detail_metabox_nonce'], '_themename_book_detail_metabox_update')) {
-            return;
-        } 
-
-      /*   if (!isset($_POST['_themename_book_detail_metabox_nonce']) || !check_admin_referer('_themename_book_detail_metabox_update', '_themename_book_detail_metabox_nonce')) {
+        if (!isset($_POST[$this->nonce_field]) || !wp_verify_nonce($_POST[$this->nonce_field], $this->nonce_action)) {
             return;
         }
- */
+
+
 
         $edit_cap = get_post_type_object($post->post_type)->cap->edit_post;
         if (!current_user_can($edit_cap, $post_id)) {
@@ -107,8 +113,10 @@ class BookDetailMetaBox
         // Save or update the meta values for the book
         $author = isset($_POST[$this->author]) ? sanitize_text_field($_POST[$this->author]) : '';
         $published_date = isset($_POST[$this->published_date]) ? sanitize_text_field($_POST[$this->published_date]) : '';
+        $layout = isset($_POST[$this->layout]) ? sanitize_text_field($_POST[$this->layout]) : '';
 
         update_post_meta($post_id, $this->author, $author);
         update_post_meta($post_id, $this->published_date, $published_date);
+        update_post_meta($post_id, $this->layout, $layout);
     }
 }
