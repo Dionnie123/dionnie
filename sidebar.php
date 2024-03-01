749aa86ec06b29789c2d452ab872de760523a1b6
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-03-02 01:47:39 +0800

    up

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,7 @@
-<aside role="complementary">
-    <?php dynamic_sidebar('primary-sidebar'); ?>
-</aside>
+<div class="card p-3">
+    <aside role="complementary">
+
+        <?php dynamic_sidebar('primary-sidebar'); ?>
+
+    </aside>
+</div>
