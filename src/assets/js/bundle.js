9a821230063d76c885892a601fe67a32f2febe37
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-03-03 00:12:38 +0800

    up

--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -1,6 +1,6 @@
 // bundle.js
 
-import "../../../node_modules/bootstrap/dist/js/bootstrap.esm.min";
+import * as bootstrap from "./components/bootstrap.esm.min";
 import "./components/slider";
 import "./components/navigation";
 console.log("bundle.js ✔️");
