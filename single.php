5e11d5ae8a04952e5f9b63e29668a4eeedc04584
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-03-03 00:43:51 +0800

    up

--- single.php
+++ single.php
@@ -10,39 +10,10 @@ get_header(); ?>
                     while (have_posts()) :
                         the_post();
                     ?>
-                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                            <header class="entry-header">
-                                <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-                                <div class="entry-meta">
-                                    <?php
-
-                                    ?>
-                                </div>
-                            </header>
-
-                            <div class="entry-content">
-                                <?php
-                                the_content();
-
-                                wp_link_pages(
-                                    array(
-                                        'before' => '<div class="page-links">' . esc_html__('Pages:', 'Your_Theme'),
-                                        'after'  => '</div>',
-                                    )
-                                );
-                                ?>
-                            </div>
-
-                            <footer class="entry-footer">
-
-                            </footer><!-- .entry-footer -->
-                        </article><!-- #post-<?php the_ID(); ?> -->
+                        <?php get_template_part('template-parts/post/content'); ?>
 
                     <?php
-                        // If comments are open or we have at least one comment, load comments template.
-                        if (comments_open() || get_comments_number()) {
-                            comments_template();
-                        }
+
 
                     endwhile; ?>
                 </div>
