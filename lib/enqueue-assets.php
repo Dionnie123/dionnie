014acf5432f7088037a941f63ef539631b7914b9
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-03-26 22:59:24 +0800

    up

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -3,7 +3,7 @@
 function _themename_assets()
 {
 
-    wp_enqueue_style('_themename-stylesheet',  get_template_directory_uri() . '/dist/assets/css/bundle.css', array(), rand(0, 9999), 'all'); //this must be the last css to be added
+    wp_enqueue_style('_themename-stylesheet',  get_template_directory_uri() . '/dist/assets/css/bundle.css', array(), 1, 'all'); //this must be the last css to be added
 
     include(get_template_directory() . '/lib/inline-css.php');
     wp_add_inline_style('_themename-stylesheet', $inline_styles);
@@ -11,7 +11,7 @@ function _themename_assets()
 
 
 
-    wp_enqueue_script('_themename-scripts',  get_template_directory_uri() . '/dist/assets/js/bundle.js', array('jquery'), rand(0, 9999), true); //this must be the last js to be added
+    wp_enqueue_script('_themename-scripts',  get_template_directory_uri() . '/dist/assets/js/bundle.js', array('jquery'), 1, true); //this must be the last js to be added
 
 
 
